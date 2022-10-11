012cabdff517abbaf2c8aeb2db11a663e40b9fe5
Sanne Crawford <s1184451@student.windesheim.nl>
2022-10-11 15:11:11 +0200

    v3

--- 7.27/7.27/7.27.cpp
+++ 7.27/7.27/7.27.cpp
@@ -1,19 +1,40 @@
-#define READ 10
-#define WRITE 11
-#define LOAD 20
-#define STORE 21
-#define ADD 30
-#define SUBTRACT 31
-#define DIVIDE 32
-#define MULTIPLY 33
-#define BRANCH 40
-#define BRANCHNEG 41
-#define BRANCHZERO 42
-#define HALT 43
 #include<stdio.h>
+
+
 
 int main(void) {
 	
+	int a;
+	int i = 0;
+	int Sum = 0;
+	int negative = 0;
+	int posative = 0;
+	int g;
+	int largest = 1;
 	
-	
+	printf("Enter the amount of numbers you want to progress:");
+	scanf_s("%d", &g);
+
+	while ( i < g) {
+
+		printf("Enter a number:");
+		scanf_s("%d", &a);
+		Sum += a;
+		i++;
+		if (a >= largest) {
+			largest = a;
+		}
+		if (a > 0) {
+			posative++;
+		}
+		if (a < 0) {
+			negative++;
+		}
+		if (i == g) {
+			printf("The sum is: %d\n", Sum);
+			printf("The amount of negative numbers: %d\nThe amount of posative: %d\n", negative, posative);
+			printf("The largest number of this set is: %d\n", largest);
+			return 0;
+		}
+	}
 }
